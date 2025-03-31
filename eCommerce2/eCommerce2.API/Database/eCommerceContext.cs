59faaff9a98e08d2ac94b400281598e026dee24b
Jean Macedo <Jeancmacedo1@gmail.com>
2025-03-31 13:36:37 -0300

    ADD - eCommerceContext.cs on Database

---
+++ eCommerce2/eCommerce2.API/Database/eCommerceContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace eCommerce2.API.Database
+{
+    public class eCommerceContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=eCommerce;Integrated Security=True;");
+        }
+    }
+}
