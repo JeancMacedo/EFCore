28fac1cfacd6b6c796bbc458be07cbbbf01455ef
Jean Macedo <Jeancmacedo1@gmail.com>
2025-03-31 13:11:25 -0300

    Add - 'HTTP´s' Controller ( UsuaariosController.cs ) in Controllers

---
+++ eCommerce2/eCommerce2.API/Controllers/UsuarioController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerce2.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+    }
+}
