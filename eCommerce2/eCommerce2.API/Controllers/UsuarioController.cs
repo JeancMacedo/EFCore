3506efbe5c25db67d3ade4e71fc91f159493755a
Jean Macedo <Jeancmacedo1@gmail.com>
2025-03-31 13:12:47 -0300

    FIX - 'HTTP´s' Controller ( UsuaariosController.cs ) in Controllers

--- eCommerce2/eCommerce2.API/Controllers/UsuarioController.cs
+++ eCommerce2/eCommerce2.API/Controllers/UsuarioController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using eCommerce2.API.Repositories;
+using eCommerce2.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace eCommerce2.API.Controllers
@@ -7,5 +9,48 @@ namespace eCommerce2.API.Controllers
     [ApiController]
     public class UsuarioController : ControllerBase
     {
+        private readonly IUsuarioRepository _repository;
+
+        public UsuarioController(IUsuarioRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var listaUsuarios = _repository.Get();
+            return Ok(listaUsuarios);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var usuario = _repository.Get(id);
+            if (usuario == null)
+                return NotFound("Não encontrado!");
+            return Ok(usuario);
+        }
+
+        [HttpPost]
+        public IActionResult Add([FromBody]Usuario usuario)
+        {
+            _repository.Add(usuario);
+            return Ok(usuario);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update([FromBody]Usuario usuario, int id)
+        {
+            _repository.Update(usuario);
+            return Ok(usuario);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _repository.Delete(id);
+            return Ok();
+        }
     }
 }
