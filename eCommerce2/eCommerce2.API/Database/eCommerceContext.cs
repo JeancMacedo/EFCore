b999e855e0c0dc60aa77824fd8e44b799179f29b
Jean Macedo <Jeancmacedo1@gmail.com>
2025-03-31 13:53:47 -0300

    FIX - Program.cs & eCommerceContext.cs & appsettings.Development.json (add directory dtbase.server)

--- eCommerce2/eCommerce2.API/Database/eCommerceContext.cs
+++ eCommerce2/eCommerce2.API/Database/eCommerceContext.cs
@@ -4,6 +4,11 @@ namespace eCommerce2.API.Database
 {
     public class eCommerceContext : DbContext
     {
+        public eCommerceContext(DbContextOptions<eCommerceContext> options) : base(options)
+        {
+
+        }
+
         #region Conexão sem distinção de ambientes de execução
         /*protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
