c71ed83be4999efb66eda3083183a92b581792d4
Jean Macedo <Jeancmacedo1@gmail.com>
2025-03-31 12:59:55 -0300

    ADD - DI (Injeção de Dependência)

--- eCommerce2/eCommerce2.API/Program.cs
+++ eCommerce2/eCommerce2.API/Program.cs
@@ -1,3 +1,5 @@
+using eCommerce2.API.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 #region ConfigureService()
 // Add services to the container.
@@ -5,6 +7,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
 #endregion
 
 var app = builder.Build();
