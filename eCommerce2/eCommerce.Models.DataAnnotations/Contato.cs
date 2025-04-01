b01931815312a5bd87784d580d9af47d6a936a95
Jean Macedo <Jeancmacedo1@gmail.com>
2025-04-01 10:21:09 -0300

    Update - Contato.cs & Usuario.cs on eCommerce.Models.DataAnnotations

--- eCommerce2/eCommerce.Models.DataAnnotations/Contato.cs
+++ eCommerce2/eCommerce.Models.DataAnnotations/Contato.cs
@@ -1,11 +1,27 @@
-﻿namespace eCommerce2.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace eCommerce2.Models
 {
     public class Contato
     {
         public int Id { get; set; }
-        public int UsuarioId { get; set; }
+
         public string? Telefone { get; set; }
         public string? Celular { get; set; }
+        /*
+         * Coluna - MER
+         * Convensão: {Modelo}+{PK} = UsuarioId
+         */
+        /*
+         * UsuarioId(MER) - FK(Propriedade do POO)
+        [ForeignKey("Usuario")]
+        */
+        public int TBUsuarioId { get; set; }
+        /*
+         * POO (Navegar)
+         * [ForeignKey("UsuarioId")]
+         */
+
         public Usuario? Usuario { get; set; } 
     }
 }
--- eCommerce2/eCommerce.Models.DataAnnotations/Usuario.cs
+++ eCommerce2/eCommerce.Models.DataAnnotations/Usuario.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace eCommerce2.Models
 {
@@ -6,7 +7,13 @@ namespace eCommerce2.Models
     [Table("TB_USUARIOS")]
     public class Usuario
     {
+        //Convensão Id - UsuarioId = PK - Identity
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int Id { get; set; }
+        //[Key]
+        //[Column("COD")]
+        //public int Codigo { get; set; }
+
         public string Nome { get; set; } = null!;
         public string Email { get; set; } = null!;
         public string? Sexo { get; set; }
@@ -16,9 +23,16 @@ namespace eCommerce2.Models
         public string CPF { get; set; } = null!;
         public string? NomeMae { get; set; }
         public string? SituacaoCadastro { get; set; }
+        
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Matricula { get; set; }
 
+        [NotMapped]
+        public bool RegistroAtivo { get; set; }
 
-        public DateTimeOffset DataCadastro { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTimeOffset DataCadastro { get; set; } // "GetDate()"
+        [ForeignKey("UsuarioId")]
         public Contato? Contato { get; set; }
         public ICollection<EnderecoEntrega>? EnderecosEntrega { get; set; }
         public ICollection<Departamento>? Departamentos { get; set; }
