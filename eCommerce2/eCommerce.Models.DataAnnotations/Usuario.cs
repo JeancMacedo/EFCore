9d7ca86f25d52191e22fd318ac03bf8bf25231d3
Jean Macedo <Jeancmacedo1@gmail.com>
2025-04-01 10:03:27 -0300

    ADD - Column (REGISTRO_GERAL) on eCommerce.Models.DataAnnotations

--- eCommerce2/eCommerce.Models.DataAnnotations/Usuario.cs
+++ eCommerce2/eCommerce.Models.DataAnnotations/Usuario.cs
@@ -1,15 +1,23 @@
-﻿namespace eCommerce2.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace eCommerce2.Models
 {
+
+    [Table("TB_USUARIOS")]
     public class Usuario
     {
         public int Id { get; set; }
         public string Nome { get; set; } = null!;
         public string Email { get; set; } = null!;
         public string? Sexo { get; set; }
+
+        [Column("REGISTRO_GERAL")]
         public string? RG { get; set; }
         public string CPF { get; set; } = null!;
         public string? NomeMae { get; set; }
         public string? SituacaoCadastro { get; set; }
+
+
         public DateTimeOffset DataCadastro { get; set; }
         public Contato? Contato { get; set; }
         public ICollection<EnderecoEntrega>? EnderecosEntrega { get; set; }
