f39d68e799c331bcde73a8f1219bcc26ba860c3a
Jean Macedo <Jeancmacedo1@gmail.com>
2025-04-03 13:39:23 -0300

    ADD - DataBase SQL to eCommerceOfficeContext.cs

---
+++ eCommerce2/eCommerce.Office/Migrations/20250403162825_BancoInicial.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eCommerce.Office.Migrations
+{
+    public partial class BancoInicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Colaboradores",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Colaboradores", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Turmas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Turmas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Veiculos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Placa = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Veiculos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Setores",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ColaboradorId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Setores", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Setores_Colaboradores_ColaboradorId",
+                        column: x => x.ColaboradorId,
+                        principalTable: "Colaboradores",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ColaboradorTurma",
+                columns: table => new
+                {
+                    ColaboradoresId = table.Column<int>(type: "int", nullable: false),
+                    TurmasId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ColaboradorTurma", x => new { x.ColaboradoresId, x.TurmasId });
+                    table.ForeignKey(
+                        name: "FK_ColaboradorTurma_Colaboradores_ColaboradoresId",
+                        column: x => x.ColaboradoresId,
+                        principalTable: "Colaboradores",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ColaboradorTurma_Turmas_TurmasId",
+                        column: x => x.TurmasId,
+                        principalTable: "Turmas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ColaboradorVeiculo",
+                columns: table => new
+                {
+                    ColaboradoresId = table.Column<int>(type: "int", nullable: false),
+                    VeiculosId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ColaboradorVeiculo", x => new { x.ColaboradoresId, x.VeiculosId });
+                    table.ForeignKey(
+                        name: "FK_ColaboradorVeiculo_Colaboradores_ColaboradoresId",
+                        column: x => x.ColaboradoresId,
+                        principalTable: "Colaboradores",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ColaboradorVeiculo_Veiculos_VeiculosId",
+                        column: x => x.VeiculosId,
+                        principalTable: "Veiculos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ColaboradoresSetores",
+                columns: table => new
+                {
+                    ColaboradorId = table.Column<int>(type: "int", nullable: false),
+                    SetorId = table.Column<int>(type: "int", nullable: false),
+                    Criado = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ColaboradoresSetores", x => new { x.SetorId, x.ColaboradorId });
+                    table.ForeignKey(
+                        name: "FK_ColaboradoresSetores_Colaboradores_ColaboradorId",
+                        column: x => x.ColaboradorId,
+                        principalTable: "Colaboradores",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ColaboradoresSetores_Setores_SetorId",
+                        column: x => x.SetorId,
+                        principalTable: "Setores",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Colaboradores",
+                columns: new[] { "Id", "Nome" },
+                values: new object[,]
+                {
+                    { 1, "José" },
+                    { 2, "Maria" },
+                    { 3, "Felipe" },
+                    { 4, "Tiago" },
+                    { 5, "Mariano" },
+                    { 6, "Jessica" },
+                    { 7, "Vivian" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Setores",
+                columns: new[] { "Id", "ColaboradorId", "Nome" },
+                values: new object[,]
+                {
+                    { 1, null, "Lógistica" },
+                    { 2, null, "Separação" },
+                    { 3, null, "Financeiro" },
+                    { 4, null, "Administrativo" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "ColaboradoresSetores",
+                columns: new[] { "ColaboradorId", "SetorId", "Criado" },
+                values: new object[,]
+                {
+                    { 1, 1, new DateTimeOffset(new DateTime(2025, 4, 3, 13, 28, 25, 586, DateTimeKind.Unspecified).AddTicks(2955), new TimeSpan(0, -3, 0, 0, 0)) },
+                    { 6, 1, new DateTimeOffset(new DateTime(2025, 4, 3, 13, 28, 25, 586, DateTimeKind.Unspecified).AddTicks(2976), new TimeSpan(0, -3, 0, 0, 0)) },
+                    { 4, 2, new DateTimeOffset(new DateTime(2025, 4, 3, 13, 28, 25, 586, DateTimeKind.Unspecified).AddTicks(2980), new TimeSpan(0, -3, 0, 0, 0)) },
+                    { 5, 2, new DateTimeOffset(new DateTime(2025, 4, 3, 13, 28, 25, 586, DateTimeKind.Unspecified).AddTicks(2978), new TimeSpan(0, -3, 0, 0, 0)) },
+                    { 7, 3, new DateTimeOffset(new DateTime(2025, 4, 3, 13, 28, 25, 586, DateTimeKind.Unspecified).AddTicks(2981), new TimeSpan(0, -3, 0, 0, 0)) },
+                    { 2, 4, new DateTimeOffset(new DateTime(2025, 4, 3, 13, 28, 25, 586, DateTimeKind.Unspecified).AddTicks(2983), new TimeSpan(0, -3, 0, 0, 0)) },
+                    { 3, 4, new DateTimeOffset(new DateTime(2025, 4, 3, 13, 28, 25, 586, DateTimeKind.Unspecified).AddTicks(2984), new TimeSpan(0, -3, 0, 0, 0)) }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ColaboradoresSetores_ColaboradorId",
+                table: "ColaboradoresSetores",
+                column: "ColaboradorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ColaboradorTurma_TurmasId",
+                table: "ColaboradorTurma",
+                column: "TurmasId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ColaboradorVeiculo_VeiculosId",
+                table: "ColaboradorVeiculo",
+                column: "VeiculosId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Setores_ColaboradorId",
+                table: "Setores",
+                column: "ColaboradorId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ColaboradoresSetores");
+
+            migrationBuilder.DropTable(
+                name: "ColaboradorTurma");
+
+            migrationBuilder.DropTable(
+                name: "ColaboradorVeiculo");
+
+            migrationBuilder.DropTable(
+                name: "Setores");
+
+            migrationBuilder.DropTable(
+                name: "Turmas");
+
+            migrationBuilder.DropTable(
+                name: "Veiculos");
+
+            migrationBuilder.DropTable(
+                name: "Colaboradores");
+        }
+    }
+}
--- eCommerce2/eCommerce.Office/Models/ColaboradorSetor.cs
+++ eCommerce2/eCommerce.Office/Models/ColaboradorSetor.cs
@@ -4,6 +4,7 @@
     {
         public int ColaboradorId { get; set; }
         public int SetorId { get; set; }
+        public DateTimeOffset Criado { get; set; }
         public Colaborador? Colaborador { get; set; }
         public Setor? Setor { get; set; }
     }
