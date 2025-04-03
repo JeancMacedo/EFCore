5ab93b4d3f4f11d787b045e3e565816d215bb832
Jean Macedo <Jeancmacedo1@gmail.com>
2025-04-03 14:02:30 -0300

    Add - Programa code (Consult - Many-To-Many with 2 One-To-Many

--- eCommerce2/eCommerce.Office/Program.cs
+++ eCommerce2/eCommerce.Office/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using eCommerce.Office;
+using Microsoft.EntityFrameworkCore;
+
+var db = new eCommerceOfficeContext();
+
+#region Many-To-Many > 2x One-To-Many
+//Setor > ColaboradoresSetores > Colaborador
+var resultado = db.Setores!.Include(a=>a.ColaboradoresSetores).ThenInclude(a=>a.Colaborador);
+foreach(var setor in resultado)
+{
+    Console.WriteLine(setor.Nome);
+    foreach(var colabSetor in setor.ColaboradoresSetores)
+    {
+        Console.WriteLine(/*colabSetor.SetorId + */" - " + colabSetor.Colaborador.Nome);
+    }
+}
+Console.ReadLine();
+#endregion
