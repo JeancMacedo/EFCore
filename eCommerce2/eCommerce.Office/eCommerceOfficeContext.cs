a3a4c1c77e54084a79d9c749dab77569f0066f33
Jean Macedo <Jeancmacedo1@gmail.com>
2025-04-03 11:19:19 -0300

    Create - eCommerceOfficeContext.cs and ADD your config for DbContext

--- eCommerce2/eCommerce.Office/eCommerceOfficeContext.cs
+++ eCommerce2/eCommerce.Office/eCommerceOfficeContext.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using eCommerce.Office.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace eCommerce.Office
 {
-    internal class eCommerceOfficeContext
+    public class eCommerceOfficeContext : DbContext
     {
+        public DbSet<Colaborador>? Colaboradores { get; set; }
+        public DbSet<Setor>? Setores { get; set; }
+        public DbSet<Turma>? Turmas { get; set; }
+        public DbSet<Veiculo>? Veiculos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=eCommerceOffice;Integrated Security=True;");
+        }
     }
 }
